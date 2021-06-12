1d4290787135cadd6fa5dee02475cb5c019a40bc
dimakrepak <krepakdima@gmail.com>
2021-06-12 14:35:26 +0300

    Add Component
    How component works.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
 
-import { AppComponent } from './app.component';
-
+import {AppComponent} from "./app.component";
+import {ServerComponent} from "./server/server.component";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
