6789e2637bc4592d47343e2533ddbaf1216bf660
dimakrepak <krepakdima@gmail.com>
2021-06-12 20:12:24 +0300

    Practicing on creation components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 
 import {AppComponent} from "./app.component";
-import {ServerComponent} from "./server/server.component";
+import {ServersComponent} from "./servers/servers.component";
+import {WarningComponent} from "./componets-create-practise/warning-alert/warning-alert.component";
+import {SuccessAlertComponent} from "./componets-create-practise/success-alert/success-alert.component";
 @NgModule({
   declarations: [
-    AppComponent, ServerComponent
+    AppComponent, ServersComponent, WarningComponent, SuccessAlertComponent
   ],
   imports: [
     BrowserModule, FormsModule
