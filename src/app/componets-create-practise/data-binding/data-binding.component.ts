0653216a021624671f5dae2c4a57899b4ed57d8b
dimakrepak <krepakdima@gmail.com>
2021-06-12 23:28:17 +0300

    practice

--- src/app/componets-create-practise/data-binding/data-binding.component.ts
+++ src/app/componets-create-practise/data-binding/data-binding.component.ts
@@ -2,22 +2,32 @@ import {Component, OnInit} from "@angular/core";
 
 @Component({selector: "app-data-binding", templateUrl: "./data-binding.component.html", styleUrls: ["./data-binding.component.css"]})
 export class DataBindingComponent implements OnInit {
-  taskId: number = 15;
-  taskStatus: string = "offline";
-  alloweNewTask: boolean = true;
-  taskCreationStatus = "No tasks";
+  // taskId: number = 15;
+  // taskStatus: string = "offline";
+  // alloweNewTask: boolean = true;
+  // taskCreationStatus = "No tasks";
+  // taskName: string = "";
+  username: string = "";
+  usernameCreationStatus = "No User";
 
   constructor() {
-    setTimeout(() => {
-      this.alloweNewTask = false;
-    }, 2000);
+    // setTimeout(() => {
+    //   this.alloweNewTask = false;
+    // }, 2000);
   }
-  getTaskStatus() {
-    return this.taskStatus;
-  }
-  handleCreateTask() {
-    this.taskCreationStatus = "Task was created";
-  }
-
   ngOnInit(): void {}
+
+  // getTaskStatus() {
+  //   return this.taskStatus;
+  // }
+  // handleCreateTask() {
+  //   this.taskCreationStatus = "Task was created! Name of the task is " + this.taskName;
+  // }
+  // handleUpdateTaskName(e : Event) {
+  //   this.taskName = (<HTMLInputElement>e.target).value;
+  // }
+  handleCreateUsername() {
+    this.usernameCreationStatus = "User Created Succefully " + this.username;
+    this.username = "";
+  }
 }
