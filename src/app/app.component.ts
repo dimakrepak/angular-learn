b5d1ef96c295a530e6cf4a858d9f815f81287e8b
dimakrepak <krepakdima@gmail.com>
2021-07-09 00:09:16 +0300

    Binding to custom properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name= 'Dima';
+  serverElements = [
+    { type: 'server', name: 'Testserver', content: 'Just a test' },
+  ];
+  name = 'Dima';
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,12 @@
-import {Component, OnInit} from "@angular/core";
+import { Component, OnInit, Input } from '@angular/core';
 
-@Component({selector: 'app-servers', templateUrl: "./servers.component.html", styleUrls: ["./servers.component.css"]})
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
 export class ServersComponent implements OnInit {
+  @Input() element: { type: string; name: string; content: string };
   constructor() {}
 
   ngOnInit(): void {}
